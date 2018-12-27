10aee7b3439b8451be29e9ffd504e22354562661
gucong <gucong@gmail.com>
2018-12-27 12:54:12 +0800

    Parser of template placeholder

--- test/literals.js
+++ test/literals.js
@@ -13,7 +13,7 @@ describe("template literals", () => {
 		});
 
 		code = code.toString();
-		expect(document.toString(), code);
+		expect(document.toString()).to.equal(code);
 		expect(document.source).to.haveOwnProperty("lang", "jsx");
 
 		expect(document.nodes).to.have.lengthOf(1);
@@ -35,7 +35,7 @@ describe("template literals", () => {
 		});
 
 		code = code.toString();
-		expect(document.toString(), code);
+		expect(document.toString()).to.equal(code);
 		expect(document.source).to.haveOwnProperty("lang", "jsx");
 
 		expect(document.nodes).to.have.lengthOf(1);
@@ -52,6 +52,139 @@ describe("template literals", () => {
 					"'blue'}",
 				].join("\n\t")
 			);
+		});
+	});
+
+	it("interpolation as the only content of a component", () => {
+		const file = require.resolve("./fixtures/interpolation-content");
+		let code = fs.readFileSync(file);
+
+		const document = syntax.parse(code, {
+			from: file,
+		});
+
+		code = code.toString();
+		expect(document.toString()).to.equal(code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+
+		expect(document.nodes).to.have.lengthOf(5);
+		document.nodes.forEach((root, i) => {
+			switch (i) {
+				case 0: {
+					expect(root.nodes).to.have.lengthOf(1);
+					root.nodes.forEach(decl => {
+						expect(decl).have.property("type", "decl");
+						expect(decl).have.property("prop", "display");
+						expect(decl).have.property("value", "inline-block");
+					});
+					return;
+				}
+				case 1:
+				case 2: {
+					expect(root.nodes).to.have.lengthOf(2);
+					expect(root.first).have.property("type", "literal");
+					expect(root.first).have.property("text", "${buttonStyles}");
+					expect(root.last).have.property("type", "decl");
+					expect(root.last).have.property("prop", "color");
+					expect(root.last).have.property("value", "red");
+					return;
+				}
+				case 3:
+				case 4: {
+					expect(root.nodes).to.have.lengthOf(2);
+					expect(root.first).have.property("type", "decl");
+					expect(root.first).have.property("prop", "color");
+					expect(root.first).have.property("value", "red");
+					expect(root.last).have.property("type", "literal");
+					expect(root.last).have.property("text", "${buttonStyles}");
+				}
+			}
+		});
+	});
+
+	it("selector", () => {
+		const file = require.resolve("./fixtures/tpl-selector");
+		let code = fs.readFileSync(file);
+
+		const document = syntax.parse(code, {
+			from: file,
+		});
+
+		code = code.toString();
+		expect(document.toString()).to.equal(code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+
+		expect(document.nodes).to.have.lengthOf(1);
+		expect(document.first.nodes).to.have.lengthOf(6);
+		document.first.nodes.forEach(rule => {
+			expect(rule).to.have.property("type", "rule");
+			expect(rule).to.have.property("selector");
+			expect(rule.selector).to.match(/(?:^|\s)\$\{selector\}(?=,|\s|$)/);
+		});
+	});
+
+	describe("decl", () => {
+		const file = require.resolve("./fixtures/tpl-decl");
+		const code = fs.readFileSync(file);
+
+		syntax.parse(code, {
+			from: file,
+		}).first.nodes.forEach(rule => {
+			it(rule.selector, () => {
+				expect(rule.nodes).to.have.lengthOf(1);
+				const decl = rule.first;
+				expect(decl).to.have.property(
+					"prop",
+					/\bprop\b/.test(rule.selector)
+						? `${/\bprefix\b/.test(rule.selector) ? "prefix-" : ""}\${prop}${/\bsuffix\b/.test(rule.selector) ? "-suffix" : ""}`
+						: "prop"
+				);
+				expect(decl).to.have.property(
+					"value",
+					/\bvalue\b/.test(rule.selector)
+						? `${/\bprefix\b/.test(rule.selector) ? "prefix-" : ""}\${value}${/\bsuffix\b/.test(rule.selector) ? "-suffix" : ""}`
+						: "value"
+				);
+			});
+		});
+	});
+
+	it("non-literals", () => {
+		const file = require.resolve("./fixtures/tpl-special");
+		let code = fs.readFileSync(file);
+
+		const document = syntax.parse(code, {
+			from: file,
+		});
+
+		code = code.toString();
+		expect(document.toString()).to.equal(code);
+		expect(document.source).to.haveOwnProperty("lang", "jsx");
+
+		document.walk(node => {
+			expect(node).to.have.property("type");
+			expect(node.type).to.not.equal("literal");
+		});
+	});
+
+	describe("template-safe-parse", () => {
+		[
+			"./fixtures/tpl-in-tpl",
+			"./fixtures/multiline-arrow-function",
+			"./fixtures/interpolation-content",
+			"./fixtures/tpl-selector",
+			"./fixtures/tpl-decl",
+			"./fixtures/tpl-special",
+		].map(file => {
+			it(file, () => {
+				file = require.resolve(file);
+				const code = fs.readFileSync(file);
+				syntax({
+					css: "safe-parser",
+				}).parse(code, {
+					from: "styled-safe-parse.js",
+				});
+			});
 		});
 	});
 });
